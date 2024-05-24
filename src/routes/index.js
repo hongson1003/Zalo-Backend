d43319ee80d2c50ae426dc7d456377db0278454a
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-05-24 11:18:46 +0700

    modify route health

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,20 @@
-import express from "express"
+import express from "express";
 import InitRoutesAuthentication from "../routes/auth.route";
-import InitRoutesUsers from '../routes/user.route';
-import InitRoutesChat from '../routes/chat.route';
+import InitRoutesUsers from "../routes/user.route";
+import InitRoutesChat from "../routes/chat.route";
 
 const router = express.Router();
 
 const configRoutes = async (app) => {
-    app.get('/', (req, res) => {
-        return res.render('index');
-    })
-    app.use('/auth', InitRoutesAuthentication(router));
-    app.use('/users', InitRoutesUsers(router));
-    app.use('/chat', InitRoutesChat(router));
-
-}
+  app.get("/health", (req, res) => {
+    return res.status(200).send({
+      status: "OK",
+      message: "Server is up and running",
+    });
+  });
+  app.use("/auth", InitRoutesAuthentication(router));
+  app.use("/users", InitRoutesUsers(router));
+  app.use("/chat", InitRoutesChat(router));
+};
 
 module.exports = configRoutes;
